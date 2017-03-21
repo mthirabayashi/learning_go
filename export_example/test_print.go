e4e514809edea98e44bb0eae99206f3d6b5fd06d
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-03-20 20:36:27 -0700

    add first export package

--- export_example/test_print.go
+++ export_example/test_print.go
@@ -1,11 +1,11 @@
-package main
+package nums
 
 import "fmt"
 
 // print_nums will not be exported because it is lowercase
 // Testprintnums will be exported because it is uppercase
 
-func main() {
+func print_nums() {
   fmt.Printf("%d - %b - %x\n", 42, 42, 42)
   fmt.Printf("%#x\n", 42)
   fmt.Printf("%#X\n", 42)
@@ -14,4 +14,8 @@ func main() {
   // %x - hexadecimal
   // %#x - hexadecimal with 0x prepended
   // %#X - upper case hexadecimal with 0x prepended
+}
+
+func Testprintnums() {
+  print_nums()
 }
--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import "github.com/mthirabayashi/initial_go_practice/export_example"
 
 func main() {
   fmt.Printf("hello, world\n")
+  // print_nums.print_nums() - cannot call because lowercase func are not exported
+  nums.Testprintnums()
 }
