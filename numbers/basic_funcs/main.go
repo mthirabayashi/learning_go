b8da7d021b7bb2036ad8ae219e7640de9b17ed6c
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-03-22 14:42:14 -0700

    add basic numberical functions with return values

---
+++ numbers/basic_funcs/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := 5
+  b := 8
+  fmt.Println(add(a,b))
+  fmt.Println(subtract(a,b))
+  fmt.Println(multiply(a,b))
+  c := 20.0
+  d := 8.0
+  fmt.Println(divide(c,d))
+}
+
+func add(x int, y int) int {
+  return x + y
+}
+func subtract(x int, y int) int {
+  return x - y
+}
+func multiply(x int, y int) int {
+  return x * y
+}
+func divide(x float64, y float64) float64 {
+  return x / y
+}
